6fdcbe473adb3c58c7731799df609c7c7155866d
Ivan Guerra <102552151+IvanIsay@users.noreply.github.com>
2026-02-16 15:45:20 -0600

    parte flask

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,44 +1,127 @@
-from fastapi import FastAPI
-from fastapi import status,HTTPException
 
-app= FastAPI()
+#Importaciones
+from fastapi import FastAPI, status, HTTPException
+import asyncio
+from typing import Optional
 
-#BD ficticia
+# Instacia del servidor
+app = FastAPI(
+    title='MI primer API',
+    description='Ivan Isay Guerra',
+    version='1.0.0'
+    )
+
+#TB ficticia
 usuarios=[
-    {"id": 1,"nombre":"ivan", "edad":37},
-    {"id": 2,"nombre":"isay", "edad":15},
-    {"id": 3,"nombre":"petra", "edad":18},
-    {"id": 4,"nombre":"ana", "edad":37}
+    {"id":1,"nombre":"Juan","edad":21},
+    {"id":2,"nombre":"Israel","edad":21},
+    {"id":3,"nombre":"Sofi","edad":21},
 ]
 
-#Endpoint home
-@app.get('/')
-def home():
-    return {'hello':'world FastAPI'}
+#Endpoints
+@app.get("/",tags=['Inicio'])
+async def bienvenida():
+    return {"mensaje": "¡Bienvenido a mi API!"}
 
-#CRUD para usuarios
-
-@app.get("/v1/usuarios/", tags=["Usuarios CRUD"])
-async def leer_usuarios():
+@app.get("/HolaMundo" ,tags=['Bienvenida Asincrona'])
+async def hola():
+    await asyncio.sleep(3) #simulacion de una peticion
     return {
-        "status": "ok",
+        "mensaje":"¡Hola Mundo FastAPI!",
+        "estatus":"200"
+            }
+    
+@app.get("/v1/parametroOb/{id}",tags=['Parametro Obligatorio'])
+async def consultaUno(id:int):
+    return {"Se encontro usuario": id}
+
+
+@app.get("/v1/parametroOp/",tags=['Parametro Opcional'])
+async def consultaTodos(id:Optional[int]=None):
+    if id is not None:
+        for usuariok in usuarios:
+            if usuariok["id"] == id:
+                return{"mensaje":"usuario encontrado","usuario":usuariok}
+        return {"mensaje":"usuario no encontrado","usuario":id}
+    else:
+        return {"mensaje":"No se proporciono id" }
+    
+
+
+@app.get("/v1/usuarios/",tags=['CRUD HTTP'])
+async def leer_usuarios( ):
+    return{
+        "status":"200",
         "total": len(usuarios),
-        "data": usuarios
+        "usuarios":usuarios
     }
     
-@app.post("/v1/usuarios/", tags=["Usuarios CRUD"], status_code=status.HTTP_201_CREATED)
-async def crear_usuario(usuario: dict):
-
+@app.post("/v1/usuarios/",tags=['CRUD HTTP'],status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario:dict):
     for usr in usuarios:
         if usr["id"] == usuario.get("id"):
             raise HTTPException(
                 status_code=400,
-                detail="El id  ya existe"
+                detail="El id ya existe"
             )
-
     usuarios.append(usuario)
-
-    return {
-        "message": "Usuario creado correctamente",
-        "data": usuario
+    return{
+        "mensaje":"Usuario Agregado",
+        "Usuario":usuario
     }
+    
+
+@app.put("/v1/usuarios/{id}", tags=["CRUD HTTP"],status_code=status.HTTP_200_OK)
+async def actualizar_usuario(id: int, usuario_actualizado: dict):
+
+    for index, usr in enumerate(usuarios):
+        if usr["id"] == id:
+
+            # Reemplazamos completamente el usuario
+            usuarios[index] = usuario_actualizado
+
+            return {
+                "message": "Usuario actualizado completamente",
+                "data": usuario_actualizado
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Usuario no encontrado"
+    )
+    
+@app.patch("/v1/usuarios/{id}", tags=["CRUD HTTP"],status_code=status.HTTP_200_OK)
+async def actualizar_parcial(id: int, datos: dict):
+
+    for usr in usuarios:
+        if usr["id"] == id:
+
+            # Actualización parcial
+            usr.update(datos)
+
+            return {
+                "message": "Usuario actualizado parcialmente",
+                "data": usr
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Usuario no encontrado"
+    )
+    
+@app.delete("/v1/usuarios/{id}", tags=["CRUD HTTP"],status_code=status.HTTP_200_OK)
+async def eliminar_usuario(id: int):
+
+    for index, usr in enumerate(usuarios):
+        if usr["id"] == id:
+
+            usuarios.pop(index)
+
+            return {
+                "message": "Usuario eliminado correctamente"
+            }
+
+    raise HTTPException(
+        status_code=404,
+        detail="Usuario no encontrado"
+    )
